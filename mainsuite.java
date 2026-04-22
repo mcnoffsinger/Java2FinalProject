a1ccb473f2f2b59c94a6d27eeaa9cb710536a8b0
mcnoffsinger <mcnoffsinger@my.waketech.edu>
2026-04-22 18:50:08 -0400

    Added main loop without vscode
    lets hope it works

--- mainsuite.java
+++ mainsuite.java
@@ -1,7 +1,7 @@
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
 
 public class mainsuite {
     static String[] E = {"E ","F ","Gb","G ","Ab","A ", "Bb", "B ", "C ","Db","D ","Eb"};
@@ -9,6 +9,7 @@ public class mainsuite {
     static String[] D = {"D ","Eb","E ","F ","Gb","G ","Ab","A ", "Bb", "B ", "C ","Db"};
     static String[] G = {"G ","Ab","A ", "Bb", "B ", "C ","Db","D ","Eb","E ","F ","Gb"};
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
     
 
@@ -31,10 +32,30 @@ public class mainsuite {
         */
         
         //main program loop
+        List<String> inputList = new ArrayList<>();
+        
+        
+        System.out.println("Hello! This is a simple porgram that outputs tabs for chords you enter. You probably won't get any use out of it, but I will.");
         while (true){
-            System.out.println("work in progress");
-            break;
+            System.out.println("Enter a chord to add or type \"exit\" to quit and see results. \nWhen typing the chords, type the capital letter (A-G), a space, min or maj, and then either a space or the number 7. \nEX: \"A min7\" or \"Dbmin \" \n");
+            
+            
+            String input = scanner.nextLine();
+            //System.out.println(input);
+            if (input.contains("exit")){
+                break;
+            }else{
+                inputList.add(input);
+            }
         }
+        List<chord> thinglist = new ArrayList<>();
+        for (String inputtedChord : inputList){
+            thinglist.add(new chord(inputtedChord, E, A, D, G));
+        }
+        tab fulltab = new tab(thinglist, E, A, D, G);
+        fulltab.printTab();
+        scanner.close();
+        
     
 
     
